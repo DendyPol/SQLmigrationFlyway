10eeb07953b311a07117eaa1b123663f937f1d26
DendyPol <Polovinko_91@icloud.com>
2023-06-06 21:05:58 +0300

    fix_tab_add_null

--- src/main/resources/db/migration/V1_0__create_country_and_city_world.sql
+++ src/main/resources/db/migration/V1_0__create_country_and_city_world.sql
@@ -1,27 +1,27 @@
 create table if not exists country (
-    id   bigint primary key unique auto_increment,
-    name varchar(120) unique not null
+  id   bigint primary key unique auto_increment,
+  name varchar(120) unique not null
 );
 
 create table if not exists region (
-    id         bigint primary key unique auto_increment,
-    name       varchar(180) not null,
-    id_country bigint       not null,
-    foreign key (id_country) references country (id)
+  id         bigint primary key unique auto_increment,
+  name       varchar(180) not null,
+  id_country bigint       not null,
+  foreign key (id_country) references country (id)
 );
 
 create table if not exists district (
-    id        bigint primary key unique auto_increment,
-    name      varchar(180) not null,
-    id_region bigint       not null,
-    foreign key (id_region) references region (id)
+  id        bigint primary key unique auto_increment,
+  name      varchar(180) not null,
+  id_region bigint       not null,
+  foreign key (id_region) references region (id)
 );
 
 create table if not exists city (
-    id          bigint primary key unique auto_increment,
-    name        varchar(180) not null,
-    id_region   bigint       not null,
-    id_district bigint       not null,
-    foreign key (id_region) references region (id),
-    foreign key (id_district) references district (id)
+  id          bigint primary key unique auto_increment,
+  name        varchar(180) not null,
+  id_region   bigint       not null,
+  id_district bigint       null,
+  foreign key (id_region) references region (id),
+  foreign key (id_district) references district (id)
 );
