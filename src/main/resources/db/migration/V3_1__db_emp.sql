a398ebecbbb7b1b043b18ced0c457f5cc4486bb2
DendyPol <Polovinko_91@icloud.com>
2023-07-03 12:02:40 +0300

    third_homework_migration_county_and_employees

---
+++ src/main/resources/db/migration/V3_1__db_emp.sql
@@ -0,0 +1,28 @@
+select avg(salary)
+from salaries
+       left join employees using (emp_no)
+       left join dept_emp on dept_emp.emp_no = employees.emp_no
+       left join departments on departments.dept_no = dept_emp.dept_no;
+
+select max(salary), concat(last_name, ' ', first_name) 'full_name'
+from employees
+       left join salaries using (emp_no)
+where salary in (select max(salary)
+                 from salaries)
+group by 2;
+
+delete employees
+from employees
+       left join salaries using (emp_no)
+where salary in (select max(salary)
+                 from salaries);
+
+select count(employees.employees.emp_no)
+from employees;
+
+select dept_name, count(dept_emp.emp_no) as count_employees, sum(salary) as money_dept
+from departments
+       left join dept_emp using (dept_no)
+       left join salaries on dept_emp.emp_no = salaries.emp_no
+group by dept_name
+order by money_dept desc;
